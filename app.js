eebc615c3f3008cbd618879b83dec9f43cf9fd67
Darshan Patel <darshanpatel25894@gmail.com>
2017-12-09 03:07:24 -0500

    (notes.js) basic import of self-authored modules

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js');
+
+var res = notes.addNote();
+console.log(res);
+
+var sum = notes.add(73, 27);
+console.log('Result :', sum);
 
 var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!\n`, function (err) {
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`, function (err) {
     if (err) {
         console.log('Unable to write to file.');
     }
