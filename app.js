84f0a33e5bc4df668617cdc849a50a1e5c889cab
Darshan Patel <darshanpatel25894@gmail.com>
2017-12-09 19:17:45 -0500

    (modules) using npm modules

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
+// var res = notes.addNote();
+// console.log(res);
 
-var sum = notes.add(73, 27);
-console.log('Result :', sum);
+// var sum = notes.add(73, 27);
+// console.log('Result :', sum);
 
-var user = os.userInfo();
+// var user = os.userInfo();
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`, function (err) {
-    if (err) {
-        console.log('Unable to write to file.');
-    }
-});
+// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.\n`, function (err) {
+//     if (err) {
+//         console.log('Unable to write to file.');
+//     }
+// });
+
+// console.log(_.isString(true))
+// console.log(_.isString('Darshan'))
+
+var filterArray = _.uniq(['Darshan', 1, 'Darshan', 1, 2, 2, 3, 4]);
+console.log(filterArray);
