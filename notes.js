718c2586c9b769d8c977a2b12d78c73eb8b18207
Darshan Patel <darshanpatel25894@gmail.com>
2017-12-09 19:26:12 -0500

    (nodemon) added nodemon for constantly running node app

--- notes.js
+++ notes.js
@@ -1,12 +1,6 @@
-// console.log('Starting notes.js');
-
-module.exports.age = 23;
+console.log('Starting notes.js');
 
 module.exports.addNote = () => {
     console.log('addNote');
     return 'New Note';
 };
-
-module.exports.add = (a, b) => {
-    return a + b
-}
