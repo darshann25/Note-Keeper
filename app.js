8394669bd4ede7db4bb76f509f1cebf11020a42e
Darshan Patel <darshanpatel25894@gmail.com>
2017-12-11 18:50:06 -0500

    (app) added commandline help

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-console.log('Starting app.')
+// console.log('Starting app.')
 
 const fs = require('fs')
 const _ = require('lodash')
@@ -6,10 +6,36 @@ const yargs = require('yargs')
 
 const notes = require('./notes.js')
 
-const argv = yargs.argv
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+}
+
+const bodyOptions = {
+    describe: 'Body of the note',
+    demand: true,
+    alias: 'b'
+}
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv
+    
 var command = argv._[0]
-console.log('Command : ', command)
-console.log('Yargs : ', argv)
+// console.log('Command : ', command)
+// console.log('Yargs : ', argv)
 
 switch (command) {
     case 'add':
